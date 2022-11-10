15a264f4017518d78c3798c62ea982451a210e22
Alex Jansen <alexjansen210@gmail.com>
2022-11-10 13:23:06 -0800

    role assignment and fetch guild member fetch

--- drivers/bot.js
+++ drivers/bot.js
@@ -1,10 +1,51 @@
-import { Client, GatewayIntentBits } from 'discord.js'
+import 'dotenv/config'
+import { Client, Events } from 'discord.js'
 
-// TODO understand gateway intents
+// TODO: understand if i need gateway intents
+const client = new Client({ intents: [] })
+let guild
 
-// TODO watch gateway for captain and player role changes
-// propogate to db and set verified/approved status accordingly
+client.once(Events.ClientReady, async (c) => {
+    console.log(`Ready! Logged in as ${c.user.tag}`)
+    guild = c.guilds.resolve(process.env.GUILD_ID)
 
-// TODO create functions for fetching, assigning, and removing roles
+    // console.log(guild.members.me.permissions.toArray());
+})
 
-// TODO create function to check tag for validity on guild
+// TODO: watch gateway for captain and player role changes
+// propogate to db and set verified/approved status accordinnpmgly
+
+export async function addRole (userSnowflake, roleSnowflake) {
+    return await guild.members.addRole({
+        user: userSnowflake,
+        role: roleSnowflake
+    })
+}
+export async function removeRole (userSnowflake, roleSnowflake) {
+    return await guild.members.removeRole({
+        user: userSnowflake,
+        role: roleSnowflake
+    })
+}
+export async function fetchMemberRoles (userSnowflake) {
+    const member = await guild.members.fetch(userSnowflake)
+    return member._roles
+}
+export async function fetchGuildRoles () {
+    const roles = await guild.roles.fetch()
+    // let roleList = roles.map((role) => [role.position, role.id, role.name]);
+    const roleList = roles.map((role) => role.id)
+    // roleList.sort((a, b) => {
+    // return b[0] - a[0];
+    // });
+    return roleList
+}
+
+export async function fetchGuildMember (username, discriminator) {
+    const members = await guild.members.fetch({ query: username })
+    return members.find((member) =>
+        member.user.discriminator.includes(discriminator)
+    )
+}
+
+client.login(process.env.DISCORD_BOT_TOKEN)
