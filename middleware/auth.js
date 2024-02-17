e32815d1117b417bd05c71fed52d43099b76e7e1
Alex Jansen <alexjansen210@gmail.com>
2024-02-16 18:23:19 -0800

    check against ID instead of roles

--- middleware/auth.js
+++ middleware/auth.js
@@ -93,7 +93,7 @@ router.get('/logout', (req, res) => {
 router.get('/me', userAuth, async (req, res) => {
   const clean = Object.assign({}, req.session.user)
   delete clean.auth
-  if (clean.roles.includes('144973321749004289')) { // hard overwrite for haazman - post removal of admin role
+  if (clean._id.equals('144973321749004289')) { // hard overwrite for haazman - post removal of admin role
     clean.admin = true
   }
   if (clean.roles.includes(adminRoleID)) {
